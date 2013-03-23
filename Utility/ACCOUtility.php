24e0ee84553ffaf6dc3eb0b143332d7275934176
Philippe Dellaert <philippe@dellaert.org>
2013-03-23 15:17:05 +0100

    small fix

--- Utility/ACCOUtility.php
+++ Utility/ACCOUtility.php
@@ -4,7 +4,7 @@ namespace Dellaert\ACCOBooklistBundle\Utility;
 class ACCOUtility {
 	public static function verifyReferer($ref,$val) {
 		preg_match("/https?:\/\/([a-zA-Z0-9\-\.]+)/", $ref, $matches);
-		if( $matches[1] == $val ) {
+		if( count($matches) > 1 && $matches[1] == $val ) {
 			return true;
 		}
 		return false;
