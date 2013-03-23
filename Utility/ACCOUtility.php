8e490b2469c761fb5043effb3681fd6510c0577c
Philippe Dellaert <philippe@dellaert.org>
2013-03-23 15:13:40 +0100

    fix

--- Utility/ACCOUtility.php
+++ Utility/ACCOUtility.php
@@ -3,7 +3,7 @@ namespace Dellaert\ACCOBooklistBundle\Utility;
 
 class ACCOUtility {
 	public static function verifyReferer($ref,$val) {
-		preg_match("/https?://([a-zA-Z0-9\-\.]+)/", $ref, $matches);
+		preg_match("/https?:\/\/([a-zA-Z0-9\-\.]+)/", $ref, $matches);
 		if( $matches[1] == $val ) {
 			return true;
 		}
