a9bbeb1be3e16becfd6248ab466c1a7a7a819728
Philippe Dellaert <philippe@dellaert.org>
2013-03-23 14:09:22 +0100

    seperate layer to avoid JS issues

---
+++ Controller/KULAPIController.php
@@ -0,0 +1,120 @@
+<?php
+namespace Dellaert\ACCOBooklistBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class APIController extends Controller {
+
+	public function listFacultiesByIdTitleAction() {
+		// Getting Faculties
+		$url = $this->container->getParameter('dellaert_acco_booklist.kulapi.base');
+		$url .= '/'.$this->getRequest()->getLocale().'/faculties-id-title';
+		$data = file_get_contents($url);
+
+		if( $data === FALSE ) {
+			$data = '';
+		}
+
+		// Returning faculties
+		$response = new Response($data);
+		$response->headers->set('Content-Type', 'application/json');
+    	return $response;
+	}
+
+	public function listLevelsByIdTitleAction($fid) {
+		// Getting Levels
+		$url = $this->container->getParameter('dellaert_acco_booklist.kulapi.base');
+		$url .= '/'.$this->getRequest()->getLocale().'/levels-id-title/'.$fid;
+		$data = file_get_contents($url);
+
+		if( $data === FALSE ) {
+			$data = '';
+		}
+
+		// Returning levels
+		$response = new Response($data);
+		$response->headers->set('Content-Type', 'application/json');
+		return $response;
+	}
+
+	public function listStudiesByIdTitleAction($fid,$lid) {
+		// Getting Studies
+		$url = $this->container->getParameter('dellaert_acco_booklist.kulapi.base');
+		$url .= '/'.$this->getRequest()->getLocale().'/studies-id-title/'.$fid.'/'.$lid;
+		$data = file_get_contents($url);
+
+		if( $data === FALSE ) {
+			$data = '';
+		}
+
+		// Returning studies
+		$response = new Response($data);
+		$response->headers->set('Content-Type', 'application/json');
+		return $response;
+	}
+
+	public function listProgramsByIdTitleAction($sid) {
+		// Getting Programs
+		$url = $this->container->getParameter('dellaert_acco_booklist.kulapi.base');
+		$url .= '/'.$this->getRequest()->getLocale().'/programs-id-title/'.$sid;
+		$data = file_get_contents($url);
+
+		if( $data === FALSE ) {
+			$data = '';
+		}
+
+		// Returning programs
+		$response = new Response($data);
+		$response->headers->set('Content-Type', 'application/json');
+		return $response;
+	}
+
+	public function listStagesByIdTitleAction($pid) {
+		// Getting Stages
+		$url = $this->container->getParameter('dellaert_acco_booklist.kulapi.base');
+		$url .= '/'.$this->getRequest()->getLocale().'/stages-id-title/'.$pid;
+		$data = file_get_contents($url);
+
+		if( $data === FALSE ) {
+			$data = '';
+		}
+
+		// Returning stages
+		$response = new Response($data);
+		$response->headers->set('Content-Type', 'application/json');
+		return $response;
+	}
+
+	public function listCoursesInLevelAction($pid,$phid) {
+		// Getting Courses
+		$url = $this->container->getParameter('dellaert_acco_booklist.kulapi.base');
+		$url .= '/'.$this->getRequest()->getLocale().'/courses-in-level/'.$pid.'/'.$phid;
+		$data = file_get_contents($url);
+
+		if( $data === FALSE ) {
+			$data = '';
+		}
+
+		// Returning Courses
+		$response = new Response($data);
+		$response->headers->set('Content-Type', 'application/json');
+		return $response;
+	}
+
+	public function listCoursesByGroupsInLevelAction($pid,$phid) {
+		// Getting Courses
+		$url = $this->container->getParameter('dellaert_acco_booklist.kulapi.base');
+		$url .= '/'.$this->getRequest()->getLocale().'/courses-by-groups-in-level/'.$pid.'/'.$phid;
+		$data = file_get_contents($url);
+
+		if( $data === FALSE ) {
+			$data = '';
+		}
+
+		// Returning Courses
+		$response = new Response($data);
+		$response->headers->set('Content-Type', 'application/json');
+		return $response;
+	}
+}
