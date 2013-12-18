27dc458e15e7fdee64e38a1f0de89c4af733ff05
Philippe Dellaert <philippe@dellaert.org>
2013-12-18 20:12:38 +0100

    Trying to support multiple schools with same XML layout

--- Controller/KULAPIController.php
+++ Controller/KULAPIController.php
@@ -7,11 +7,11 @@ use Dellaert\ACCOBooklistBundle\Utility\ACCOUtility;
 
 class KULAPIController extends Controller {
 
-	public function listFacultiesByIdTitleAction() {
+	public function listSchoolsByIdTitleAction() {
 		if( ACCOUtility::verifyReferer($this->getRequest()->server->get('HTTP_REFERER'),$this->getRequest()->server->get('SERVER_NAME')) ) {
 			// Getting Faculties
 			$url = $this->container->getParameter('dellaert_acco_booklist.kulapi.base');
-			$url .= '/'.$this->getRequest()->getLocale().'/faculties-id-title';
+			$url .= '/'.$this->getRequest()->getLocale().'/schools-id-title/';
 			$data = file_get_contents($url);
 
 			if( $data === FALSE ) {
@@ -30,11 +30,34 @@ class KULAPIController extends Controller {
 	    return $response;
 	}
 
-	public function listLevelsByIdTitleAction($fid) {
+	public function listFacultiesByIdTitleAction($scid) {
+		if( ACCOUtility::verifyReferer($this->getRequest()->server->get('HTTP_REFERER'),$this->getRequest()->server->get('SERVER_NAME')) ) {
+			// Getting Faculties
+			$url = $this->container->getParameter('dellaert_acco_booklist.kulapi.base');
+			$url .= '/'.$this->getRequest()->getLocale().'/faculties-id-title/'.$scid;
+			$data = file_get_contents($url);
+
+			if( $data === FALSE ) {
+				$data = '';
+			}
+
+
+
+			// Returning faculties
+			$response = new Response($data);
+			$response->headers->set('Content-Type', 'application/json');
+    	} else {
+			$response = new Response();
+    		$response->setStatusCode('403');
+    	}
+	    return $response;
+	}
+
+	public function listLevelsByIdTitleAction($scid,$fid) {
 		if( ACCOUtility::verifyReferer($this->getRequest()->server->get('HTTP_REFERER'),$this->getRequest()->server->get('SERVER_NAME')) ) {
 			// Getting Levels
 			$url = $this->container->getParameter('dellaert_acco_booklist.kulapi.base');
-			$url .= '/'.$this->getRequest()->getLocale().'/levels-id-title/'.$fid;
+			$url .= '/'.$this->getRequest()->getLocale().'/levels-id-title/'.$scid.'/'.$fid;
 			$data = file_get_contents($url);
 
 			if( $data === FALSE ) {
@@ -51,11 +74,11 @@ class KULAPIController extends Controller {
 		return $response;
 	}
 
-	public function listStudiesByIdTitleAction($fid,$lid) {
+	public function listStudiesByIdTitleAction($scid,$fid,$lid) {
 		if( ACCOUtility::verifyReferer($this->getRequest()->server->get('HTTP_REFERER'),$this->getRequest()->server->get('SERVER_NAME')) ) {
 			// Getting Studies
 			$url = $this->container->getParameter('dellaert_acco_booklist.kulapi.base');
-			$url .= '/'.$this->getRequest()->getLocale().'/studies-id-title/'.$fid.'/'.$lid;
+			$url .= '/'.$this->getRequest()->getLocale().'/studies-id-title/'.$scid.'/'.$fid.'/'.$lid;
 			$data = file_get_contents($url);
 
 			if( $data === FALSE ) {
@@ -72,11 +95,11 @@ class KULAPIController extends Controller {
 		return $response;
 	}
 
-	public function listProgramsByIdTitleAction($sid) {
+	public function listProgramsByIdTitleAction($scid,$sid) {
 		if( ACCOUtility::verifyReferer($this->getRequest()->server->get('HTTP_REFERER'),$this->getRequest()->server->get('SERVER_NAME')) ) {
 			// Getting Programs
 			$url = $this->container->getParameter('dellaert_acco_booklist.kulapi.base');
-			$url .= '/'.$this->getRequest()->getLocale().'/programs-id-title/'.$sid;
+			$url .= '/'.$this->getRequest()->getLocale().'/programs-id-title/'.$scid.'/'.$sid;
 			$data = file_get_contents($url);
 
 			if( $data === FALSE ) {
@@ -93,11 +116,11 @@ class KULAPIController extends Controller {
 		return $response;
 	}
 
-	public function listStagesByIdTitleAction($pid) {
+	public function listStagesByIdTitleAction($scid,$pid) {
 		if( ACCOUtility::verifyReferer($this->getRequest()->server->get('HTTP_REFERER'),$this->getRequest()->server->get('SERVER_NAME')) ) {
 			// Getting Stages
 			$url = $this->container->getParameter('dellaert_acco_booklist.kulapi.base');
-			$url .= '/'.$this->getRequest()->getLocale().'/stages-id-title/'.$pid;
+			$url .= '/'.$this->getRequest()->getLocale().'/stages-id-title/'.$scid.'/'.$pid;
 			$data = file_get_contents($url);
 
 			if( $data === FALSE ) {
@@ -114,11 +137,11 @@ class KULAPIController extends Controller {
 		return $response;
 	}
 
-	public function listCoursesInLevelAction($pid,$phid) {
+	public function listCoursesInLevelAction($scid,$pid,$phid) {
 		if( ACCOUtility::verifyReferer($this->getRequest()->server->get('HTTP_REFERER'),$this->getRequest()->server->get('SERVER_NAME')) ) {
 			// Getting Courses
 			$url = $this->container->getParameter('dellaert_acco_booklist.kulapi.base');
-			$url .= '/'.$this->getRequest()->getLocale().'/courses-in-level/'.$pid.'/'.$phid;
+			$url .= '/'.$this->getRequest()->getLocale().'/courses-in-level/'.$scid.'/'.$pid.'/'.$phid;
 			$data = file_get_contents($url);
 
 			if( $data === FALSE ) {
@@ -135,11 +158,11 @@ class KULAPIController extends Controller {
 		return $response;
 	}
 
-	public function listCoursesByGroupsInLevelAction($pid,$phid) {
+	public function listCoursesByGroupsInLevelAction($scid,$pid,$phid) {
 		if( ACCOUtility::verifyReferer($this->getRequest()->server->get('HTTP_REFERER'),$this->getRequest()->server->get('SERVER_NAME')) ) {
 			// Getting Courses
 			$url = $this->container->getParameter('dellaert_acco_booklist.kulapi.base');
-			$url .= '/'.$this->getRequest()->getLocale().'/courses-by-groups-in-level/'.$pid.'/'.$phid;
+			$url .= '/'.$this->getRequest()->getLocale().'/courses-by-groups-in-level/'.$scid.'/'.$pid.'/'.$phid;
 			$data = file_get_contents($url);
 
 			if( $data === FALSE ) {
