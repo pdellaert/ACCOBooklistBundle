f862be58612b3f778f031839dca7c5135fad794b
Philippe Dellaert <philippe@dellaert.org>
2014-01-27 22:59:08 +0100

    Setting up export web interface

--- Controller/MainController.php
+++ Controller/MainController.php
@@ -9,4 +9,29 @@ class MainController extends Controller
 	public function indexAction() {
 		return $this->render('DellaertACCOBooklistBundle:Main:index.html.twig');
 	}
+
+	public function generateCourseMaterialOverviewAction($locale) {
+		$form = $this->createFormBuilder()
+			->add('school','choice',array(
+				'choices'=>array('-1'=>'Even geduld...'),
+				'expanded'=>false,
+				'multiple'=>false,
+				'required'=>true))
+			->add('faculty','choice',array(
+				'choices'=>array('-1'=>'Even geduld...'),
+				'expanded'=>false,
+				'multiple'=>false,
+				'required'=>true))
+			->add('level','choice',array(
+				'choices'=>array('-1'=>'Even geduld...'),
+				'expanded'=>false,
+				'multiple'=>false,
+				'required'=>true));
+		$results = array();
+		$showResults = false;
+		if( $request->getMethod() == 'POST' ) {
+			// TODO: Do the generating stuff
+		}
+		return $this->render('DellaertACCOBooklistBundle:Main:course_material_overview.html.twig',array('locale'=>$locale,'form'=>$form->createView(),'results'=>$results,'showResults'=>$showResults));
+	}
 }
