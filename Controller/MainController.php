0cdd5cc79168ecb67834010b12192999ac333c71
Philippe Dellaert <philippe@dellaert.org>
2014-01-28 00:14:55 +0100

    generating output

--- Controller/MainController.php
+++ Controller/MainController.php
@@ -31,12 +31,135 @@ class MainController extends Controller
 				'choices'=>array('-1'=>'Even geduld...'),
 				'expanded'=>false,
 				'multiple'=>false,
-				'required'=>true));
+				'required'=>true))
+			->getForm();
 		$results = array();
 		$showResults = false;
 		if( $request->getMethod() == 'POST' ) {
-			// TODO: Do the generating stuff
+			$form->handleRequest($request);
+			if( $form->isValid() ) {
+				$showResults = true;
+				$scid = $form->get('school');
+				$fid = $form->get('faculty');
+				$lid = $form->get('level');
+				$locale = $request->getLocale();
+
+				$studies = APIUtility::getLiveStudiesByIdTitle($this->container,$locale,$scid,$fid,$lid);
+				foreach($studies as $study) {
+					$programs = APIUtility::getLiveProgramsByIdTitle($this->container,$locale,$scid,$study['id']);
+					foreach($programs as $program) {
+						$stages = APIUtility::getLiveStagesByIdTitle($this->container,$locale,$scid,$program['id']);
+						foreach($stages as $stage) {
+							switch($stage['id']) {
+								case '1':
+									$ftxt = '1';
+									break;
+								case '2':
+									$ftxt = '2';
+									break;
+								case '3':
+									$ftxt = '3';
+									break;
+								case '4':
+									$ftxt = '4';
+									break;
+								case '5':
+									$ftxt = '5';
+									break;
+								case '0':
+								default:
+									$ftxt = 'geen';
+									break;
+							}
+							$courses = APIUtility::getLiveCoursesInLevel($this->container,$locale,$scid,$program['id'],$stage['id'],1);
+							$programTxt = preg_replace('/\s+/',' ',$program['title'].'('.$program['studypoints'].' sp.)');
+							foreach($courses as $course) {
+								switch($course['mandatory']) {
+									case 'J':
+									case 'Y':
+										$mtxt = 'verplicht';
+										break;
+									default:
+										$mtxt = 'keuze';
+										break;
+								}
+								switch($course['period']) {
+									case '1':
+										$ptxt = '1';
+										break;
+									case '2':
+										$ptxt = '2';
+										break;
+									case '3':
+									default:
+										$ptxt = '1+2';
+										break;
+								}
+
+								$courseDetails = APIUtility::getLiveCourseDetails($this->container,$course['original_language'],$scid,$course['course_id']);
+								$courseMaterial = '';
+								foreach( $courseDetails['teaching_activities'] as $teaching_activity ) {
+									$courseMaterial .= strip_tags($teaching_activity['course_material']).' - ';
+								}
+								$courseMaterial = substr($courseMaterial, 0, -3);
+
+								$line = array(
+									'last_edit'		=>	data("d/m/Y"),
+									'school'		=>	APIUtility::$schools[$scid][0],
+									'program'		=>	$programTxt,
+									'year'			=>	$ftxt,
+									'period'			=>	$ptxt,
+									'course'		=>	preg_replace('/\s+/',' ',$course['title']),
+									'course_id'		=>	$course['course_id'],
+									'mandatory'		=>	$mtxt,
+									'material'		=>	$courseMaterial,
+									'student_nr'	=> 	''
+								);
+
+								$teachers = $course['teachers'];
+								if( count($teachers) == 0 ) {
+									$line['teacher1_firstname'] = 'Niet';
+									$line['teacher1_name'] = 'Toegewezen';
+									$line['teacher1_email'] = '';
+									$line['teacher1_phone'] = '';
+									$line['teacher2_firstname'] = '';
+									$line['teacher2_name'] = '';
+									$line['teacher2_email'] = '';
+									$line['teacher2_phone'] = '';
+									$line['teacher3_firstname'] = '';
+									$line['teacher3_name'] = '';
+									$line['teacher3_email'] = '';
+									$line['teacher3_phone'] = '';
+								}
+
+								if( count($teachers) > 0 ) {
+									$line['teacher1_firstname'] = preg_replace('/\s+/',' ',$teachers[0]['firstname']);
+									$line['teacher1_name'] = preg_replace('/\s+/',' ',$teachers[0]['lastname']);
+									$line['teacher1_email'] = '';
+									$line['teacher1_phone'] = '';
+								}
+
+								if( count($teachers) > 1 ) {
+									$line['teacher2_firstname'] = preg_replace('/\s+/',' ',$teachers[1]['firstname']);
+									$line['teacher2_name'] = preg_replace('/\s+/',' ',$teachers[1]['lastname']);
+									$line['teacher2_email'] = '';
+									$line['teacher2_phone'] = '';
+								}
+
+								if( count($teachers) > 2 ) {
+									$line['teacher3_firstname'] = preg_replace('/\s+/',' ',$teachers[2]['firstname']);
+									$line['teacher3_name'] = preg_replace('/\s+/',' ',$teachers[2]['lastname']);
+									$line['teacher3_email'] = '';
+									$line['teacher3_phone'] = '';
+								}
+
+								$results[] = $line;
+							}
+						}
+					}
+				}
+			}
 		}
-		return $this->render('DellaertACCOBooklistBundle:Main:course_material_overview.html.twig',array('form'=>$form->getForm()->createView(),'results'=>$results,'showResults'=>$showResults,'scid'=>$scid,'fid'=>$fid,'lid'=>$lid));
+		return $this->render('DellaertACCOBooklistBundle:Main:course_material_overview.html.twig',array('form'=>$form->createView(),'results'=>$results,'showResults'=>$showResults,'scid'=>$scid,'fid'=>$fid,'lid'=>$lid));
 	}
 }
